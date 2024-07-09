6b51fa00bcfdfb7d96fb286b5bf46c1f47ba4e4d
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-07-09 20:25:56 +0600

    complete sponsor section

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 const MainLayout = () => {
     return (
         <div className="">
             <Navbar />
             <Outlet />
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </div>
     );
 };
--- src/Page/Home/Sponsor/Sponsor.jsx
+++ src/Page/Home/Sponsor/Sponsor.jsx
@@ -1,8 +1,59 @@
+import 'swiper/css/pagination';
+import { Autoplay, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Sponsor = () => {
+
+
+    const sponsorList = [
+        { imgUrl: "/src/assets/images/sponsor/01.png", },
+        { imgUrl: "/src/assets/images/sponsor/02.png", },
+        { imgUrl: "/src/assets/images/sponsor/03.png", },
+        { imgUrl: "/src/assets/images/sponsor/04.png", },
+        { imgUrl: "/src/assets/images/sponsor/05.png", },
+        { imgUrl: "/src/assets/images/sponsor/06.png", },
+    ];
+
+
     return (
-        <div>
-            
+        <div className=' px-32 py-10 bg-gray-100'>
+            <Swiper
+                slidesPerView={2}
+                spaceBetween={20}
+                autoplay={
+                    {
+                        deley: 2000,
+                        disableOnInteraction: false
+                    }
+                }
+                breakpoints={{
+                    640: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 40,
+                    },
+                    1024: {
+                        slidesPerView: 5,
+                        spaceBetween: 50,
+                    },
+                }}
+                modules={[Autoplay]}
+                className="mySwiper"
+            >
+                {
+                    sponsorList.map((item, i) => (
+                        <SwiperSlide key={i}>
+                            <div>
+                                <img src={item.imgUrl} alt="" />
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+
+            </Swiper>
         </div>
     );
 };
